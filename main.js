9333cb3ac0bac6164adf84a4b5fa3bc4a6e86541
Alex Lebedev <alexey.broadcast@gmail.com>
2015-08-06 21:59:38 +0300

    [+] New game function [+] MDL

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 (function () {
     "use strict"
-    var scene = new Scene(20, 25);
+    var scene;
+    var loopId;
 
-    window.addEventListener("keydown", scene.onKeyPressed.bind(scene), true);
-    window.addEventListener("touchstart", scene.onTouchStarted.bind(scene), true);
-    window.addEventListener("touchend", scene.onTouchEnded.bind(scene), true);
+    function newGame() {
+        scene = new Scene(20, 25);
+        window.addEventListener("keydown", scene.onKeyPressed.bind(scene), true);
+        window.addEventListener("touchstart", scene.onTouchStarted.bind(scene), true);
+        window.addEventListener("touchend", scene.onTouchEnded.bind(scene), true);
 
-    var loopId = setInterval(function () { 
+        if (loopId)
+            clearInterval(loopId);
+
+        loopId = setInterval(function () {
             if (!scene.onTick())
                 clearInterval(loopId);
         }, 300);
+    }
+
+    document.getElementById('btn-new').onclick = newGame;
+
+    newGame();
 }) ();  
